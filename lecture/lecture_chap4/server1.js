aca3cabd674b39f3ae1253bc3ffc4df114ffb8b6
1000zoo <cjswldn99@gmail.com>
2022-01-19 20:33:41 +0900

    01-19

--- lecture/lecture_chap4/server1.js
+++ lecture/lecture_chap4/server1.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset-utf-8'});
+    res.writeHead(200, { 'Content-Type': 'text/html; charset-utf-8'}); //html임을 인지하지 못하는 브라우저 경우
     res.write('<h1>Hello Node!</h1>');
     res.write('<p>Hello server</p>');
     res.end('<p>Hello 1000zoo</p>');
@@ -13,3 +13,10 @@ server.on('listening', () => {
 server.on('error', (error) => {
     console.error(error);
 })
+// const server_ = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/html; charset-utf-8'}); //html임을 인지하지 못하는 브라우저 경우
+//     res.write('<h1>Hello Node!</h1>');
+//     res.write('<p>Hello server</p>');
+//     res.end('<p>Hello 1000zoo</p>');
+// })
+//     .listen(8081);
